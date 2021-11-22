803ba22bf0b99bd74e5f70a42ee99df7259b7d58
tianran-xi <xli832@gatech.edu>
2021-11-22 13:59:09 -0500

    Update a folder called templated and provide the wells-forgo template

---
+++ template/wells.py
@@ -0,0 +1,164 @@
+DKIM-Signature: v=1; a=rsa-sha256; c=simple/simple; d=notify.wellsfargo.com;
+	s=2011-05-wfb; t=1637208842;
+	bh=D4mpwqr3JXf18TYm7IOdXPZ/EjmratFMeUcso2fbcdA=;
+	h=Date:From:To:Subject;
+	b=PKkQ2sfGYUcvGpecMBDtjZV+oaMD3XTJAFww500G3atA0cd23s67ERqVacK+inZP4
+	 kRt6jOdP3EBwIt+hvJk9sq6CX4sSN9WkrB7+T1FonP1bjEvD1d2t8YDKZHCgztXAZm
+	 9r9u5haBQcJkiTlslhX2K3KlKwaJD1Avae2VBugc=
+Date: Fri, 12 Jun 2021 14:33:28 +0000 (UTC)
+From: Wells Fargo Online <alerts@notify.wellsfargo.com>,<allen_chenyilun@163com>
+To: stormytroll81@outlook.com
+Message-ID: <753408022.15014274.1637208842182@mn2-122a-ixb-al.wellsfargo.com>
+Subject: Wells Fargo online statement ready to view
+MIME-Version: 1.0
+Content-Type: multipart/mixed; 
+	boundary="----=_Part_15014272_1842680365.1637208842182"
+UOW_ID: 2641b-150-ade3-103e5e6f29-a2656bd0_6195c69e_5b8f-431e9||63594748333
+Return-Path: alerts@notify.wellsfargo.com
+X-MS-Exchange-Organization-ExpirationStartTime: 18 Nov 2021 04:14:03.4306
+ (UTC)
+--_000_BN8PR07MB6833D4F449F895BFC01BA547F89E9BN8PR07MB6833namp_
+Content-Type: text/html; charset="iso-8859-1"
+Content-Transfer-Encoding: quoted-printable
+
+<html>
+<head>
+<meta http-equiv=3D"Content-Type" content=3D"text/html; charset=3Diso-8859-=
+1">
+<style type=3D"text/css" style=3D"display:none;"> P {margin-top:0;margin-bo=
+ttom:0;} </style>
+</head>
+<body dir=3D"ltr">
+<div style=3D"font-family: Calibri, Arial, Helvetica, sans-serif; font-size=
+: 12pt; color: rgb(0, 0, 0);">
+<br>
+</div>
+<div>
+<div style=3D"font-family: Calibri, Arial, Helvetica, sans-serif; font-size=
+: 12pt; color: rgb(0, 0, 0);">
+<br>
+</div>
+<div id=3D"appendonsend"></div>
+<hr tabindex=3D"-1" style=3D"display:inline-block; width:98%">
+<div id=3D"divRplyFwdMsg" dir=3D"ltr"><font face=3D"Calibri, sans-serif" co=
+lor=3D"#000000" style=3D"font-size:11pt"><b>From:</b> Wells Fargo Online &l=
+t;alerts@notify.wellsfargo.com&gt;<br>
+<b>Sent:</b> Wednesday, November 17, 2021 11:14 PM<br>
+<b>To:</b> Li, Xi &lt;xli832@gatech.edu&gt;<br>
+<b>Subject:</b> Wells Fargo online statement ready to view</font>
+<div>&nbsp;</div>
+</div>
+<div>
+<div id=3D"x_container" style=3D"margin:0px 0px 0px 0px">
+<table role=3D"presentation" cellpadding=3D"0" cellspacing=3D"0" style=3D"w=
+idth:600px; background-color:#D71E28; height:42px; border-bottom:4px solid =
+#FCC60A">
+<tbody>
+<tr>
+<td style=3D"max-width:600px">
+<div align=3D"left" id=3D"x_c26img" style=3D"margin-top:11px; margin-bottom=
+:11px; margin-left:30px; text-align:left">
+<a href=3D"https://www.wellsfargo.com"><img id=3D"x_mastlogo" border=3D"0" =
+alt=3D"Wells Fargo logo" width=3D"148" class=3D"x_center-on-narrow" style=
+=3D"font-family:Wells Fargo Sans,Arial,Helvetica,sans-serif; font-size:13px=
+; line-height:18px; color:#ffffff" src=3D"https://static.wellsfargo.com/mes=
+sagecenter/static/images/masthead_wf_logo.png"></a></div>
+</td>
+</tr>
+</tbody>
+</table>
+<table role=3D"presentation" cellpadding=3D"0" cellspacing=3D"0" border=3D"=
+0" style=3D"width:600px; font-family:arial,helvetica,sans-serif; font-size:=
+12px">
+<tbody>
+<tr>
+<td>
+<h2 style=3D"margin:24px 30px 24px 30px; font-size:19px; font-family:arial,=
+helvetica,sans-serif">
+Your new statement is now available online</h2>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+The new statement for your Wells Fargo deposit account XXXXXX7402 is now av=
+ailable to view online.
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+To view your statement:
+<ol>
+<li>Sign on to <a href=3D"https://www.youtube.com/watch?v=dQdQw4w9WgXcQ" style=3D"font-size:12px; color:#336699">
+Statements and Documents</a>.</li><li>Select <strong>Statements and Disclos=
+ures</strong>.</li><li>Choose your account from the dropdown menu.</li></ol=
+>
+</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+If you are receiving COVID-19 payment assistance, your payment may not be d=
+ue at this time.</div>
+<div id=3D"x_c11text" style=3D"margin:0 30px 15px 30px; font-family:arial,h=
+elvetica,sans-serif; font-size:12px">
+If you have questions about your account, please refer to the contact infor=
+mation on your statement. For questions about viewing your statements onlin=
+e, Wells Fargo Customer Service is available 24 hours a day, 7 days a week.=
+ Call us at 1-800-956-4442.</div>
+</td>
+</tr>
+</tbody>
+</table>
+<table role=3D"presentation" cellspacing=3D"0" cellpadding=3D"0" style=3D"w=
+idth:600px; font-family:arial,helvetica,sans-serif; font-size:12px; border-=
+top:1px solid #B5ADAD; background-color:#F4F0ED">
+<tbody>
+<tr>
+<td colspan=3D"2">
+<div id=3D"x_wfFooter" style=3D"margin:10px 30px 16px 30px; vertical-align:=
+top"><a href=3D"https://https://www.youtube.com/watch?v=dQw4w9WgXcQ" style=3D"font-size:12px; font-f=
+amily:arial,helvetica,sans-serif; margin-right:16px; color:#336699"><strong=
+>wellsfargo.com</strong></a> |
+<a href=3D"https://www.wellsfargo.com/privacy-security/fraud" style=3D"font=
+-size:12px; font-family:arial,helvetica,sans-serif; margin-left:16px; margi=
+n-right:16px; color:#336699">
+Security Center</a> | <a href=3D"https://www.wellsfargo.com/help/contact-us=
+/" style=3D"font-size:12px; font-family:arial,helvetica,sans-serif; margin-=
+left:16px; color:#336699">
+Contact Us</a></div>
+<div id=3D"x_c26" style=3D"margin:16px 30px 16px 30px; font-size:12px; colo=
+r:#3B3331">
+<strong>Please do not reply to this automated email.</strong> </div>
+<div id=3D"x_c26" style=3D"margin:16px 30px 10px 30px; font-size:12px; colo=
+r:#3B3331">
+2641b-150-ade3-103e5e6f29-a2656bd0_6195c69e_5b8f-431e9&nbsp;&nbsp;</div>
+</td>
+</tr>
+</tbody>
+</table>
+<img height=3D"1" width=3D"1" alt=3D"" src=3D"https://ort.wellsfargo.com/op=
+enratetracking/alertsemail?uow_id=3D2641b-150-ade3-103e5e6f29-a2656bd0_6195=
+c69e_5b8f-431e9">
+</div>
+</div>
+</div>
+</body>
+</html>
+
+--_000_BN8PR07MB6833D4F449F895BFC01BA547F89E9BN8PR07MB6833namp_--
